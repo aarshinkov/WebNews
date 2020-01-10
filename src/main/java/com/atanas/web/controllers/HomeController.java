4eec8e1b43e1dc0c01926a5e5ccd8b798ef4666d
Atanas Arshinkov <a.arshinkov97@gmail.com>
2020-01-11 00:16:32 +0200

    Added logback.xml configuration

--- src/main/java/com/atanas/web/controllers/HomeController.java
+++ src/main/java/com/atanas/web/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.atanas.web.controllers;
 
+import org.slf4j.*;
 import org.springframework.stereotype.*;
 import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.*;
@@ -7,11 +8,16 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class HomeController
 {
+  private final Logger log = LoggerFactory.getLogger(getClass());
+
   @GetMapping(value = "/")
   public String home(Model model)
   {
+    log.debug("In home method");
+    log.error("Some error");
+
     model.addAttribute("globalMenu", "home");
-    
+
     return "home";
   }
 }
